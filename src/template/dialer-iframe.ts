fd2985f5d2c95e59a8f7b824e8e5796bcd778ca7
Himanshu Bhardwaz <himanshu@saaslabs.co>
2024-05-09 15:54:41 +0530

    Updated dialer width

--- src/template/dialer-iframe.ts
+++ src/template/dialer-iframe.ts
@@ -4,12 +4,12 @@ export default function getJustcallDialerIframe(document: Document) {
   const iframeElement = document.createElement("iframe");
 
   iframeElement.src = IFRAME_URL;
-  iframeElement.width = "300px";
+  iframeElement.width = "365px";
   iframeElement.height = "610px";
   iframeElement.allow = IFRAME_ALLOWED_PERMISSIONS;
 
   iframeElement.style.border = "1px solid #e0e0e0";
-  iframeElement.style.borderRadius = "5px";
+  iframeElement.style.borderRadius = "10px";
   iframeElement.style.boxShadow = "0 2px 5px rgba(0, 0, 0, 0.1)";
 
   return iframeElement;
